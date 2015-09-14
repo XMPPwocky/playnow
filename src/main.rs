ed146563c927a085c2d4747af8d042184c0f12aa
XMPPwocky <nttheis@gmail.com>
2015-09-14 16:29:41 -0700

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate iron;
+extern crate router;
+extern crate logger;
+
+use iron::prelude::*;
+use iron::status;
+
+const SITEADDRESS: &'static str = "localhost:8080";
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = router::Router::new();
+
+    router.get("/", mainpage);
+
+    Iron::new(router).http(SITEADDRESS).unwrap();
 }
+
+fn mainpage(_req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello world!")))
+}
+
