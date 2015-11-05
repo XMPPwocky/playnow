7d7a9c1f4162ab10c0a24619d140e190bbf73724
XMPPwocky <nttheis@gmail.com>
2015-11-05 13:24:40 -0800

    woops

--- postgres_setup.sql
+++ postgres_setup.sql
@@ -5,4 +5,17 @@ CREATE TABLE servers (
 );
 
 CREATE INDEX ON servers (steamid64);
-CREATE INDEX ON servers (last_known_ip);
+CREATE INDEX ON servers (last_known_ip) WHERE steamid64 IS NULL;
+
+CREATE TABLE player_server_relationships (
+	id serial primary key not null unique,
+
+	player_steamid64 bigint not null,
+	server_id serial references servers(id) not null,
+	
+	preferred bool not null
+);
+
+CREATE TABLE player_preferences (
+	steamid64 primary key not null unique,
+);
